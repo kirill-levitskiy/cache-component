998cd3a12c678e8ea3bd7bccf45a928c8f2cc7ff
Kirill <kyrylo.levytskyi@elastic.io>
2020-03-26 17:16:55 +0200

    :)

--- spec/storeObjects.spec.js
+++ spec/storeObjects.spec.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-expressions */
 
-const fs = require('fs');
 const { expect } = require('chai');
 const sinon = require('sinon');
 const nock = require('nock');
@@ -8,11 +7,7 @@ const { messages } = require('elasticio-node');
 const logger = require('@elastic.io/component-logger')();
 const action = require('../lib/actions/storeObjects');
 
-const cfg = {
-  resourceServerUrl: 'http://maester-service.platform.svc.cluster.local:3002',
-  apiKeyHeaderName: 'Authorization',
-  apiKeyHeaderValue: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRJZCI6IjU2YzIwN2FkYjkxMjExODFlNjUwYzBlZiIsImNvbnRyYWN0SWQiOiI1YzIyMGQ3YTIxMzI0MDAwMTI0YzljYjgiLCJ3b3Jrc3BhY2VJZCI6IjVjMjIwZDdhMjEzMjQwMDAxMjRjOWNiYSIsImZsb3dJZCI6IjVlN2NjMmQ4OTkwMTI0MDAxNTQzOWUyNSIsInVzZXJJZCI6IjVjMjIwZDdhMjEzMjQwMDAxMjRjOWNiNyIsImlhdCI6MTU4NTIzNDY0OX0.B0C8kt5DL7U-FgvJWTbD0MlpH_6zLeWu3EA1RW8svN4',
-};
+const cfg = {};
 
 const msg = {
   body: {
@@ -34,10 +29,8 @@ describe('store objects', () => {
   });
 
   before(async () => {
-    if (fs.existsSync('.env')) {
-      // eslint-disable-next-line global-require
-      require('dotenv').config();
-    }
+    process.env.ELASTICIO_OBJECT_STORAGE_URI = 'http://maester-service.platform.svc.cluster.local:3002';
+    process.env.ELASTICIO_OBJECT_STORAGE_TOKEN = 'token';
 
     lastCall = sinon.stub(messages, 'newMessageWithBody')
       .returns(Promise.resolve());
